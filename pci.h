6ffd66baea22017361fa53137a9e694847255dae
Grant Kurtz <grnt426@csh.rit.edu>
2012-04-16 15:47:59 -0400

    Started the PCI module.

---
+++ pci.h
@@ -0,0 +1,21 @@
+
+
+/**
+ * Port information for talking to the PCI bus
+ * Source: http://wiki.osdev.org/PCI#Configuration_Mechanism_.231
+ */
+#define CONF_ACC	0xCF8
+#define CONFIG_ACCESS	CONF_ACC
+#define	CONF_DAT	0xCFC
+#define CONFIG_DATA		CONF_DAT
+
+/**
+ * Class Code information for identifying the tpye of device found
+ * Source: http://wiki.osdev.org/PCI#Configuration_Mechanism_.231
+ */
+#define NO_CLASS	0x00
+#define	MASS_STOR	0x01
+#define NET_CONT	0x02
+#define DISP_CONT	0x03
+
+void _pci_probe_devices( void );
