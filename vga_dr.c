5a5d5f94e22ef3793e6c19979f4127d20349ebbd
Gideon Williams <gaw5951@rit.edu>
2012-04-17 14:31:03 -0400

    Initial vga module def

---
+++ vga_dr.c
@@ -0,0 +1,43 @@
+/*
+** VGA Driver initialization code
+*/
+
+#define	__KERNEL__20113__
+
+#include "headers.h"
+
+#include "vga_dr.h"
+
+/*
+** PRIVATE DEFINITIONS
+*/
+
+/*
+** PRIVATE DATA TYPES
+*/
+
+/*
+** PRIVATE GLOBAL VARIABLES
+*/
+
+/*
+** PUBLIC GLOBAL VARIABLES
+*/
+
+/*
+** PRIVATE FUNCTIONS
+*/
+
+/*
+** PUBLIC FUNCTIONS
+*/
+
+/*
+** _vga_init()
+**
+** initialize the vga module
+*/
+
+void _vga_init( void ) {
+
+}
