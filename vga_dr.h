4c8ae3108fc6de547ab681d69cdec3cb83ce0229
Gideon Williams <gaw5951@rit.edu>
2012-04-17 14:31:03 -0400

    Initial vga module def

---
+++ vga_dr.h
@@ -0,0 +1,43 @@
+/*
+** VGA Driver initialization code
+*/
+
+#ifndef _VGA_H
+#define _VGA_H
+
+#include "headers.h"
+
+/*
+** General (C and/or assembly) definitions
+*/
+
+#ifndef __ASM__20113__
+
+/*
+** Start of C-only definitions
+*/
+
+/*
+** Types
+*/
+
+/*
+** Globals
+*/
+
+/*
+** Prototypes
+*/
+
+/*
+** _vga_init()
+**
+** initialize the VGA driver module
+*/
+
+void _vga_init( void );
+
+
+#endif // ACM
+
+#endif // VGA
