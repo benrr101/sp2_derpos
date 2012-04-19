2ca7a52a13be3103d500a6a63cd84720d7cb6001
Benjamin Russell <benrr101@csh.rit.edu>
2012-04-19 13:10:46 -0400

    Getting closer to identifying the drive

--- sata.c
+++ sata.c
@@ -25,8 +25,30 @@ Uint16 _sata_get_bar(Uint16 bus, Uint16 device, Uint16 func, Uint16 offset) {
 	//	31:16	- Reserved
 	//	15:3	- Base Address		
 	//	 2:1	- Reserved
-	//	   0	- Hardwired to 1	<- Not needed, so and it off
+	//	   0	- Hardwired to 1	<- Not needed, so AND it off
 	return (reg & 0xFFFFFFFE);
+}
+
+Uint8 _sata_read_reg(IDEChannel channel, IDERegs reg) {
+	// Are we reading a register that is on the command or control?
+	if(reg > IDE_REG_STATUS) {
+		// We're reading from control.
+		return __inb(channel.control + (reg >> 2));
+	} else {
+		// We're reading from command
+		return __inb(channel.command + reg);
+	}
+}
+
+void _sata_write_reg(IDEChannel channel, IDERegs reg, Uint8 payload) {
+	// Are we writing to a register on command or control?
+	if(reg > IDE_REG_STATUS) {
+		// Writing to control
+		__outb(channel.control + (reg >> 2), payload);
+	} else {
+		// Writing to command
+		__outb(channel.command, payload);
+	}
 }
 
 void _sata_initialize() {
@@ -38,13 +60,6 @@ void _sata_initialize() {
 	ideChannels[ATA_PORT_CHANPRI].control = _sata_get_bar(SATA_PCI_BUS, SATA_PCI_DEVICE, SATA_PCI_FUNCTION, SATA_PCI_REG_PCTRL);
 	ideChannels[ATA_PORT_CHANSEC].command = _sata_get_bar(SATA_PCI_BUS, SATA_PCI_DEVICE, SATA_PCI_FUNCTION, SATA_PCI_REG_SCMD);
 	ideChannels[ATA_PORT_CHANSEC].control = _sata_get_bar(SATA_PCI_BUS, SATA_PCI_DEVICE, SATA_PCI_FUNCTION, SATA_PCI_REG_SCTRL);
-
-	// Point the primary channel at a good spot for IO (address stolen from lspci)
-	//_pci_config_writel(SATA_PCI_BUS, SATA_PCI_DEVICE, SATA_PCI_FUNCTION, SATA_PCI_REG_PCMD, 0x78701);
-	//_pci_config_writel(SATA_PCI_BUS, SATA_PCI_DEVICE, SATA_PCI_FUNCTION, SATA_PCI_REG_PCTRL, 0x78681);
-
-	// Point the secondary channel at a good spot for IO (address stolen from lspci)
-	//_pci_config_writel(SATA_PCI_BUS, SATA_PCI_DEVICE, SATA_PCI_FUNCTION, SATA_PCI_REG_SCMD, 0x78581);
 }
 
 void _sata_probe() {
@@ -52,14 +67,22 @@ void _sata_probe() {
 	// Initialize the sata and the channel info
 	_sata_initialize();
 
-	// Let's set up the primary master first as a test
-	Uint8 device = 0;
-	Uint8 channel;
-	IDECommandReg q;
-	for(channel = 0; channel <= ATA_PORT_CHANSEC; channel++) {
-		// Let's try to read in some of a IDECommand struct?
-		q.statusCommand = __inb(ideChannels[channel].command + 0x7);
-		c_printf("SATA Channel %x Status Register: 0x%x\n", channel, q.statusCommand);
+	// Let's probe the devices on the channels
+	Uint8 dev;
+	Uint8 chan;
+
+	// Iterate over the channels to probe
+	for(chan = ATA_PORT_CHANPRI; chan <= ATA_PORT_CHANSEC; chan++) {
+		// Iterate over each device on the channel
+		for(dev = ATA_PORT_CHANMAST; dev <= ATA_PORT_CHANSLAV; dev++) {
+			// Tell the controller which drive we want
+			_sata_write_reg(ideChannels[chan], IDE_REG_DRIVESEL, dev);
+			
+			// Read in the status register
+			c_printf("SATA Channel: %x, Device: %x, Status: 0x%x, Sectors: %d\n",
+				chan, dev, _sata_read_reg(ideChannels[chan], IDE_REG_STATUS),
+				_sata_read_reg(ideChannels[chan], IDE_REG_SECCOUNT1));
+		}	
 	}
 	
 	__panic("HOT DAMN!");
