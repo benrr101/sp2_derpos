353a71e64ab4b56f3131b388399ca0f4cb1fa8aa
Benjamin Russell <brr1922@rit.edu>
2012-04-24 15:55:16 -0400

    Aparrently that guy from os wiki dev was a genius

--- sata.c
+++ sata.c
@@ -29,40 +29,60 @@ Uint16 _sata_get_bar(Uint16 bus, Uint16 device, Uint16 func, Uint16 offset) {
 	return (reg & 0xFFFFFFFE);
 }
 
+
+/**
+ * Reads a byte from the specified ide channel register. THAR BE MAGIC IN HERE.
+ * @param	IDEChannel channel	The channel to read from
+ * @param	IDEReg reg			The register on the channel to read from
+ * @source	http://wiki.osdev.org/IDE
+ */
 Uint8 _sata_read_reg(IDEChannel channel, IDERegs reg) {
-	if(reg < IDE_REG_SECCOUNT1) {
-		// Reading from command
-		return __inb(channel.command + reg);
-	} else if(reg < IDE_REG_ALTSTATUS) {
-		// Reading from special command
+	Uint8 result;
+	if(reg > 0x07 && reg < 0x0C) {
 		_sata_write_reg(channel, IDE_REG_CONTROL, 0x80);
-		Uint8 r = __inb(channel.command + reg - 0x06);
-		_sata_write_reg(channel, IDE_REG_CONTROL, 0x0);
-		return r;
-	} else if(reg <= IDE_REG_DEVADDR) {
-		// Reading from control
-		return __inb(channel.control + reg - 0x0A);
-	} else {
-		// Reading from bus master
-		return __inb(channel.busmast + reg - 0x0E);
 	}
+
+	if(reg < 0x08) {
+		result = __inb(channel.command + reg - 0x00);
+	} else if(reg < 0x0C) {
+		result = __inb(channel.command  + reg - 0x06);
+	} else if(reg < 0x0E) {
+		result = __inb(channel.control  + reg - 0x0A);
+	} else if (reg < 0x16) {
+		result = __inb(channel.busmast + reg - 0x0E);
+	}
+   
+	if(reg > 0x07 && reg < 0x0C) {
+		_sata_write_reg(channel, IDE_REG_CONTROL, 0x00);
+	}
+   
+	return result;
 }
 
+/**
+ * Writes a byte to the specified ide channel reg. THAR BE MAGIC HERE.
+ * @param	IDEChannel channel	The channel to write to
+ * @param	IDERegs reg			The register on the channel to write to
+ * @param	Uint8 payload		The byte to write the the register
+ * @source	http://wiki.osdev.org/IDE
+ */
 void _sata_write_reg(IDEChannel channel, IDERegs reg, Uint8 payload) {
-	if(reg < IDE_REG_SECCOUNT1) {
-		// Reading from command
-		__outb(channel.command + reg, payload);
-	} else if(reg < IDE_REG_ALTSTATUS) {
-		// Reading from special command
+	if(reg > 0x07 && reg < 0x0C) {
 		_sata_write_reg(channel, IDE_REG_CONTROL, 0x80);
-		__outb(channel.command + reg - 0x06, payload);
-		_sata_write_reg(channel, IDE_REG_CONTROL, 0x0);
-	} else if(reg <= IDE_REG_DEVADDR) {
-		// Reading from control
-		__outb(channel.control + reg - 0x0A, payload);
-	} else {
-		// Reading from bus master
+	} 
+
+	if(reg < 0x08) {
+		__outb(channel.command  + reg - 0x00, payload);
+	} else if(reg < 0x0C) {
+		__outb(channel.command  + reg - 0x06, payload);
+	} else if(reg < 0x0E) {
+		__outb(channel.control  + reg - 0x0A, payload);
+    } else if(reg < 0x16) {
 		__outb(channel.busmast + reg - 0x0E, payload);
+	}
+
+	if(reg > 0x07 && reg < 0x0C) {
+		_sata_write_reg(channel, IDE_REG_CONTROL, 0x0);
 	}
 }
 
@@ -77,6 +97,11 @@ void _sata_initialize() {
 	ideChannels[ATA_PORT_CHANSEC].control = _sata_get_bar(SATA_PCI_BUS, SATA_PCI_DEVICE, SATA_PCI_FUNCTION, SATA_PCI_REG_SCTRL);
 	ideChannels[ATA_PORT_CHANPRI].busmast = _sata_get_bar(SATA_PCI_BUS, SATA_PCI_DEVICE, SATA_PCI_FUNCTION, SATA_PCI_REG_BMAST);
 	ideChannels[ATA_PORT_CHANSEC].busmast = ideChannels[ATA_PORT_CHANPRI].busmast + 0x8;
+}
+
+void _sata_wait() {
+	volatile int i;
+	for(i = 0; i < 100000000; i++);
 }
 
 void _sata_probe() {
@@ -98,11 +123,10 @@ void _sata_probe() {
 		for(dev = ATA_PORT_CHANMAST; dev <= ATA_PORT_CHANSLAV; dev++) {
 			// Tell the controller which drive we want
 			_sata_write_reg(ideChannels[chan], IDE_REG_DRIVESEL, 0xA0 | (dev << 4));
-			int i;
-			for(i = 0; i < 200000000; i++) { continue; }
+			_sata_wait();
 
 			_sata_write_reg(ideChannels[chan], IDE_REG_COMMAND, IDE_CMD_IDENTIFY);
-			for(i = 0; i < 200000000; i++) { continue; }
+			_sata_wait();
 			
 			// Read in the status register
 			c_printf("SATA Channel: %x, Device: %x, Status: 0x%x, Sectors: %d\n",
