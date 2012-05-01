118d2887051057c07dfb48b1b16d3156937aa7da
Gideon Williams <gaw5951@rit.edu>
2012-05-01 14:14:38 -0400

    Saving before I go back.

--- vga_dr.c
+++ vga_dr.c
@@ -49,69 +49,64 @@ static MODE_INFO *vga_mode_info;
 
 void _vga_init( void ) {
 	//Check the contents of the information extracted from REAL MODE 
-	vga_vesa_info = (VESA_INFO *)(VESA_INFO_ADDR << 1);
-	vga_mode_info = (MODE_INFO *)(VGA__INFO_ADDR << 1);
+	vga_vesa_info = (VESA_INFO *)(VESA_INFO_ADDR);
+	vga_mode_info = (MODE_INFO *)(VGA__INFO_ADDR);
 	
-	c_printf("\nVESA_INFO(%x):\n", vga_vesa_info);
-	c_printf("-version: %d\n", vga_vesa_info->VESAVersion);
-	c_printf("-VideoModePtr: %x\n", vga_vesa_info->VideoModePtr);
-	c_printf("-TtlMem: %d, %d\n", vga_vesa_info->TotalMemory, (vga_vesa_info->TotalMemory * 64)/1024);
-	c_printf("\nMODE_INFO(%x):\n", vga_mode_info);
-	c_printf("-ModeAttr: %x\n", vga_mode_info->ModeAttributes);
-	c_printf("-MemModel: %x\n", vga_mode_info->MemoryModel);
-	c_printf("-WinSize: %d\n", vga_mode_info->WinSize);
-	c_printf("-WinSize (X, Y): (%d, %d)\n", vga_mode_info->XResolution, vga_mode_info->YResolution);
-	c_printf("-CharSize (X, Y): (%d, %d)\n", vga_mode_info->XCharSize, vga_mode_info->YCharSize);
-	c_printf("-WinFuncPrt: %x\n", vga_mode_info->WinFuncPtr);
+	c_printf("\n(2)VESA_INFO: %x", vga_vesa_info);
+	c_printf("\nVer.  ModePtr    ttlmem    Cap");
+	c_printf("\n%x | %x | %d - %d | %d %d %d %d",
+		vga_vesa_info->VESAVersion, 
+		vga_vesa_info->VideoModePtr,  
+		vga_vesa_info->TotalMemory, 
+		((vga_vesa_info->TotalMemory * 64)/1024),
+		vga_vesa_info->Capabilities[0],
+		vga_vesa_info->Capabilities[1],
+		vga_vesa_info->Capabilities[2],
+		vga_vesa_info->Capabilities[3]
+		);
+	c_printf("\nMODE_INFO(%x):", vga_mode_info);
+	c_printf("\nModeAttr memModel  WinSize  Xres YRes XChar YChar");
+	c_printf("\n%x | %x | %x | (%d, %d) | (%d, %d)", 
+		vga_mode_info->ModeAttributes, 
+		vga_mode_info->MemoryModel, 
+		vga_mode_info->WinSize, 
+		vga_mode_info->XResolution, 
+		vga_mode_info->YResolution,
+		vga_mode_info->XCharSize,
+		vga_mode_info->YCharSize
+		);
+	c_printf("\n-WinFuncPrt: %x\n", vga_mode_info->WinFuncPtr);
 	c_printf("-PhysBasePrt: %x\n", vga_mode_info->PhysBasePtr);
+	c_printf("\nLin B/sl L #page  Rs  RFp Gs GFp Bs BFp Rsvds RsvdFp");
+	c_printf("\n%d | %d         | %x | %x | %x | %x | %x | %x | %x | %x ", 
+		vga_mode_info->LinbytesPerScanLine, 
+		vga_mode_info->LinNumberOfImagePages, 
+		vga_mode_info->LinRedMaskSize, 
+		vga_mode_info->LinRedFieldPosition,
+		vga_mode_info->LinGreenMaskSize,
+		vga_mode_info->LinGreenFieldPosition, 
+		vga_mode_info->LinBlueMaskSize, 
+		vga_mode_info->LinBlueFieldPosition,
+		vga_mode_info->LinRsvdMaskSize,
+		vga_mode_info->LinRsvdFieldPosition
+		);
+
 	draw();
 }
 
 void draw2( void ) {
-    /*regs.h.ah = 0x0C;   function 0Ch = pixel plot
-    regs.h.al = color;
-    regs.x.cx = x;       x location, from 0..319  
-    regs.x.dx = y;       y location, from 0..199  
-    int86(0x10,&regs,&regs);
-    */
-    asm(
-    "xorl %eax, %eax;"
-    "movb $0x0C, %ah;"
-    "movb $0x04, %al;"
-    "movw $0x14, %cx;"
-    "movw $0x14, %dx;"
-    "int $0x10;"
-    );
     return;
 }
 void back2text( void ) { 
-
-    asm(
-    "xorl   %eax, %eax;"
-    "movw   $0x03, %ax;"
-    "int    $0x10;"
-    );
     return;
-    /*  __dpmi_regs regs;
-        regs.x.ax = 0x13; 
-        Mode 0x13 is VGA 320x200x256, 0x3 is TEXT 80x25
-        __dpmi_int(0x10, ); same as real-mode 
-        return;
-     */
+
 }
 void vga256( void ) {
-
     return;
-    /*  __dpmi_regs regs;
-        regs.x.ax = 0x13; 
-        Mode 0x13 is VGA 320x200x256, 0x3 is TEXT 80x25
-        __dpmi_int(0x10, ); same as real-mode 
-        return;
-     */
 }
 
 void draw( void ) {
-	Uint32 *ptr = (Uint32*)(vga_mode_info->PhysBasePtr << 1);
+	Uint32 *ptr = (Uint32*)(vga_mode_info->PhysBasePtr);
     int i = 0;
     int j = 0;
 
