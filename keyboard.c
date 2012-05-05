a75288e857d749a888b0ba11946decfc58e3474e
Grant Kurtz <grnt426@csh.rit.edu>
2012-05-05 17:10:55 -0400

    Still working with the keyboad driver initialization.

--- keyboard.c
+++ keyboard.c
@@ -8,8 +8,125 @@
  *
  */
 
+#include "headers.h"
 #include "ps2.h"
+#include "startup.h"
+#include "keyboard.h"
 
-void _ps2_keyboard_init(){
-		
+void _ps2_keyboard_init( void ){
+
+	
+	// install our ISR
+	__install_isr( PS2_K_VEC, _ps2_keyboard_isr );
+
+	// Disable the Keyboard Interface
+	_ps2_keyboard_clear();
+	__outb( PS2_STAT, 0xF5 );
+	_ps2_keyboard_clear();
+	__outb( PS2_STAT, 0xA7 );
+	
+	// clear the output buffer
+	__inb( PS2_PORT );
+
+
+	_ps2_keyboard_clear();
+	__outb( PS2_STAT, 0xAE );
+	
+	// Enable Interrupts
+	_ps2_keyboard_clear();
+	__outb( PS2_STAT, 0x20 );
+	_ps2_keyboard_ready();
+	Uint resp = ( __inb(0x60) | 1 );
+	_ps2_keyboard_clear();
+	__outb( PS2_STAT, 0x60 );
+	_ps2_keyboard_clear();
+	__outb( 0x60, resp );
+
+	/*
+	// Set delay
+	_ps2_keyboard_clear();
+	__outb( PS2_STAT, PS2_K_TYPD );
+	c_printf("RCVD: 0x%x\n", _ps2_keyboard_read()); // Acknowledge
+	_ps2_keyboard_ready();
+	__outb( PS2_PORT, 20 );
+	c_printf("RCVD: 0x%x\n", _ps2_keyboard_read()); // Acknowledge
+	*/
+	
+	_ps2_keyboard_write( 0xF6 );
+	_ps2_keyboard_read();  //Acknowledge
+
+	// Enable
+	_ps2_keyboard_clear();
+	while( 1 )
+		;
+	__outb( PS2_STAT, 0xF4 );
+	c_printf("RCVD: 0x%x\n", _ps2_keyboard_read()); // Acknowledge
+	
+
+	// Set Typematic Delay
+	/*
+	_ps2_keyboard_clear();
+	__outb( PS2_STAT, PS2_K_TYPD );
+	c_printf("RCVD: 0x%x\n", _ps2_keyboard_read()); // Acknowledge
+	_ps2_keyboard_ready();
+	__outb( PS2_PORT, 20 );
+	c_printf("RCVD: 0x%x\n", _ps2_keyboard_read()); // Acknowledge
+	*/
+
+	// reset the keyboard
+	/*
+	_ps2_keyboard_clear();
+	__outb( PS2_STAT, 0xFF );
+	_ps2_keyboard_ready();
+	c_printf("RCVD: 0x%x\n", __inb( PS2_PORT ));
+	*/
+
+
+
+	/*	
+	// Try turning on the caps-lock light
+	_ps2_keyboard_clear();
+	__outb( PS2_K_LED, PS2_K_CAPS );
+	c_puts( "Waiting for argument buffer to be clear...\n" );
+	_ps2_keyboard_ready();
+	c_printf("RCVD: 0x%x\n",__inb( PS2_PORT )); // Acknowledge
+	_ps2_keyboard_ready();
+	__outb( PS2_PORT, PS2_K_CAPS | PS2_K_NUML | PS2_K_SCRL );
+	*/
 }
+
+void _ps2_keyboard_ready( void ){
+	while( (__inb(PS2_STAT) & 0x1) == 1 )
+		;
+}
+
+Uint _ps2_keyboard_read( void ){
+	
+	_ps2_keyboard_ready();
+	return __inb( PS2_PORT );
+}
+
+void _ps2_keyboard_write( Uint command ){
+	
+	_ps2_keyboard_clear();
+
+	// let the PS2 controller know we want to send a command to the mouse
+	//c_printf("Letting Mouse know we are sending a command...\n");
+	__outb( PS2_STAT, 0xD4 );
+	_ps2_keyboard_clear();
+
+	// Now, send the mouse our command
+	//c_printf("Sending Command: 0x%x...\n", ( b & 0xFF) );
+	__outb( PS2_PORT, command );
+}
+
+void _ps2_keyboard_clear( void ){
+	while ( (__inb(PS2_STAT) & 16) == 0 )
+		;
+}
+
+void _ps2_keyboard_isr( int vec, int code ){
+	c_puts( "GOT AN NTERRUPTS!!!!!" );
+}
+
+
--- keyboard.h
+++ keyboard.h
@@ -4,17 +4,29 @@
 // Commands
 // Source: http://wiki.osdev.org/PS2_Keyboard
 #define PS2_K_LED	0xED
+#define PS2_K_TYPD	0xF3
+#define PS2_K_SET_TYPEMATIC_DELAY	PS2_K_TYPD
 
 // Argument Data
-#define PS2_K_SHFT	0x00
-#define PS2_K_NUML	0x01
-#define PS2_K_CAPS	0x02
+// LED Light Control
+#define PS2_K_SCRL	0x01
+#define PS2_K_NUML	0x02
+#define PS2_K_CAPS	0x04
+
+// Control Flags
+#define PS2_K_IBH	0x10
+#define PS2_K_Inhibited		PS2_K_IBH
 
 
 // IRQs
 #define PS2_K_VEC	0x09
 
-void _ps2_keyboard_init();
+void _ps2_keyboard_init( void );
+void _ps2_keyboard_ready( void );
+void _ps2_keyboard_clear( void );
+void _ps2_keyboard_isr( int vec, int code );
+Uint _ps2_keyboard_read( void );
+void _ps2_keyboard_write( Uint command );
 
 
 #endif
