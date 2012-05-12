34181d7674fc44706a52f73f7edebd69cf683cb6
Sean McDermott <smm1221@rit.edu>
2012-05-12 19:23:34 -0400

    Bitmap initialized

--- vmem.c
+++ vmem.c
@@ -11,6 +11,8 @@
 #include "startup.h"
 #include "vmem.h"
 
+#define TRUE    1
+#define FALSE   0
 
 /*
 ** _vmem_init()
@@ -24,8 +26,12 @@ void _vmem_init( void )
 
 Uint32 _vmem_first_4mb( void )
 {
-	//strip the last 12 bytes then increment by page to get next valid page
-	_vmem_page_dir = (Uint32*)(( ( __get_end() >> ( PAGE_BITS -1) ) << ( PAGE_BITS - 1) ) + PAGE_SIZE);
+	if( __get_end() >= PAGE_TABLE_SIZE - sizeof(Uint32) *2  ){
+		__panic( "Houston we have a problem, the kernel is too fat");
+	}
+
+	_vmem_page_dir = (Uint32*)((__get_end() & PAGE_ADDRESS_LOC) + PAGE_SIZE);
+	c_printf( "%x\n", (__get_end() & PAGE_ADDRESS_LOC) + PAGE_SIZE ) ;
 
 	int i;
 	for(i = 0; i < 1024; i++ )
@@ -44,7 +50,7 @@ Uint32 _vmem_first_4mb( void )
 		address = address + PAGE_SIZE;
 	}
 
-	_vmem_page_dir[0] = (int) pageTableStart | PAGE_DIR_READ | PAGE_DIR_PRESENT;
+	_vmem_page_dir[0] = (Uint32) pageTableStart | PAGE_DIR_READ | PAGE_DIR_PRESENT;
 	_vmem_turnon((Uint32)_vmem_page_dir);
 
 #ifdef _VMEM_DEBUG
@@ -76,20 +82,56 @@ void _vmem_init_bitmap( Uint32 addr )
 		addr = addr + PAGE_SIZE;
 	}
 	
-	_vmem_page_dir[1] = (int) pageTableStart | PAGE_DIR_READ | PAGE_DIR_PRESENT;
+	_vmem_page_dir[1] = (Uint32) pageTableStart | PAGE_DIR_READ | PAGE_DIR_PRESENT;
 
-	for( i = 0; i < 1024; i++ ){
+	for( i = 0; i < (PAGE_TABLE_SIZE/32); i++ )
+	{
 		_vmem_bitmap_start[i] = 0;
 	}
 
-	_vmem_bitmap_start[0] = 0xC0000000;
+	for( i = 0; i < 0x400; i++)
+	{
+		_vmem_set_bit( _vmem_bitmap_start, i/32, i%32, 1);
+	}
 
 #ifdef _VMEM_DEBUG
+	c_printf("Bitmap ending init addr: %x \n", _vmem_bitmap_start );
 	c_printf(" %x : ", PAGE_TABLE_SIZE);
 	c_printf(" %x : ", PAGE_SIZE);
 	c_printf( "Bitmap table dir %x \n", pageTableStart);
 	c_printf( "Ending of bitmap table is %x \n", addr);
+	c_printf("0: %x \n", _vmem_page_dir[0] );
+	c_printf("1: %x \n", _vmem_page_dir[1] );
+	c_printf("3: %x %x\n", 0x34000, *((Uint32*)0x34000 ));
+	c_printf("3: %x %x\n", 0x35000, *((Uint32*)0x35000 ));
+	c_printf("3: %x %x\n", 0x36000, *((Uint32*)0x36000 ));
+	c_printf("6: %x %x\n", 0x37000, *((Uint32*)0x37000 ));
+	c_printf("7: %x %x\n", 0x400000, *((Uint32*)0x400000 ));
 	c_printf( "Bitmap entry 0 is %x \n", _vmem_bitmap_start[0]);
-	c_printf( "Bitmap entry 1 is %x \n", _vmem_bitmap_start[1]);
+	c_printf( "Bitmap entry 0 is %x \n", _vmem_read_bit( _vmem_bitmap_start, 0, 0));
+	c_printf( "Bitmap entry 1 is %x \n", _vmem_read_bit( _vmem_bitmap_start, 0, 1));
+	c_printf( "Bitmap entry 2 is %x \n", _vmem_read_bit( _vmem_bitmap_start, 0, 2));
+	c_printf( "Bitmap entry n is %x \n", _vmem_read_bit( _vmem_bitmap_start, 31, 31));
+	c_printf( "Bitmap entry n is %x \n", _vmem_read_bit( _vmem_bitmap_start, 32, 0));
 #endif	
+}
+
+Int8 _vmem_read_bit( Uint32* address, Uint16 index, Int8 index2 )
+{
+	return ( address[index] >> index2 ) & 0x1;
+
+}
+
+void _vmem_set_bit( Uint32* address, Uint16 index, Int8 index2, Int8 value )
+{
+	address[index] |=  value << index2;
+}
+
+void _vmem_set_address( Uint32 address )
+{
+	Uint32 new = 0x0;
+	new = address / 0x1000;
+	Uint32 index = new / 32;
+	Uint32 index2 = new % 32;
+	c_printf( "Bitmap entry n is %x \n", 0x2000);
 }
