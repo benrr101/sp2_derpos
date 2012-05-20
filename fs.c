ed36fb6f98e9ca72d84af7f9895a80cf2b0974bd
Benjamin Russell <benrr101@csh.rit.edu>
2012-05-20 15:15:09 -0400

    Fixed huge bug with allocating sectors past 8

--- fs.c
+++ fs.c
@@ -192,11 +192,10 @@ Uint32 _fs_find_empty_sector(MountPoint *mp) {
 		_ata_read_sector(mp->device, i, &s);
 		
 		// Start iterating over the bits in the bitfield
-		for(j = 0; j < FS_BT_SIZE; j++) {
+		for(j = 0; j < FS_BT_SIZE * 8; j++) {
 			// Which byte do we need 
 			byte = FS_BT_OFFSET + (FS_BT_END - (FS_BT_OFFSET + (j / 8)));
 			byte -= 1;
-			c_printf("Looking at byte %d %d\n", j, byte);
 
 			// Grab that byte and check if the bit we need is allocated
 			if(((s[byte] >> (j % 8)) & FS_BT_ALLOCATED) == 0) {
@@ -540,7 +539,8 @@ void _fs_toggle_sector(MountPoint *mp, Uint32 sector) {
 	_ata_read_sector(mp->device, ibAddr, &s);
 
 	// Toggle the appropriate sector bit
-	Uint8 mask  = FS_BT_ALLOCATED << ((sector % FS_SECT_PER_IB) % 8 - 1);
+	sector--;
+	Uint8 mask  = FS_BT_ALLOCATED << ((sector % FS_SECT_PER_IB) % 8);
 	Uint16 byte = FS_BT_END - ((sector % FS_SECT_PER_IB) / 8) - 1;
 	s[byte] ^= mask;
 
