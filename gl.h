25804ac127ea06c436813010823420d49563f07a
Gideon Williams <gaw5951@rit.edu>
2012-05-21 17:26:58 -0400

    Working. Font renders.

--- gl.h
+++ gl.h
@@ -27,16 +27,26 @@ void draw_scr_5( void );
 void draw_scr_6( void );
 void draw_scr_7( void );
 void draw_scr_8( void );
+void draw_scr_9( void );
+void draw_scr_10( void );
 
-void draw_pixel(Uint32 x, Uint32 y, pixel p);
-void draw_line(Uint32 x, Uint32 y, Uint32 x2, Uint32 y2, pixel p);
-void set_pixel(Uint32 x, Uint32 y, pixel p, screen_info* curr_si);
+Uint32 pix_to_color( pixel p );
+void draw_rect( Uint32 x1, Uint32 y1, Uint32 x2, Uint32 y2, pixel p );
+void draw_line( Uint32 x, Uint32 y, Uint32 x2, Uint32 y2, pixel p );
+
+void draw_pixel( Uint32 x, Uint32 y, pixel p );
+void set_pixel_int( Uint32 x, Uint32 y, Uint32 p, screen_info* curr_si );
+void set_pixel( Uint32 x, Uint32 y, pixel p, screen_info* curr_si );
 
 //draw char, make the x component count down so that we can flip the characters.
-void draw_character( char c, Uint32 x, Uint32 y, pixel p);
+void draw_character( char c, Uint32 x, Uint32 y, pixel p );
+
 //string is easy, make a loop that is the strlen and then draw one bit line at a
 //time using the outer loop for the character height and the inner loop as the
 //character selector (a,b,c,d....etc)
-void draw_string(char* str, Uint32 x, Uint32 y, pixel p);
 
+//draw chars for loop
+void draw_string( char* str, Uint32 x, Uint32 y, pixel p );
+//scanning
+void draw_string_s( char* str, Uint32 x, Uint32 y, pixel p );
 #endif //_GL_H_
