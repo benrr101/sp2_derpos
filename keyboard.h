f959d08fa58522e781b64b270b2f78a3dd534a31
Grant Kurtz <th3guys2@gmail.com>
2012-05-23 12:52:31 -0400

    Cleaned up the keyboard class and got rid of a lot of shit.

--- keyboard.h
+++ keyboard.h
@@ -7,22 +7,6 @@
 // Constraints on Input
 #define TOTAL_IO_REQS	12
 
-// Commands
-// Source: http://wiki.osdev.org/PS2_Keyboard
-#define PS2_K_LED	0xED
-#define PS2_K_TYPD	0xF3
-#define PS2_K_SET_TYPEMATIC_DELAY	PS2_K_TYPD
-
-// Argument Data
-// LED Light Control
-#define PS2_K_SCRL	0x01
-#define PS2_K_NUML	0x02
-#define PS2_K_CAPS	0x04
-
-// Control Flags
-#define PS2_K_IBH	0x10
-#define PS2_K_Inhibited		PS2_K_IBH
-
 // Special Keys
 #define PS2_KEY_RELEASE		0x80
 #define PS2_KEY_CTRL_P		0x1D
@@ -62,11 +46,7 @@ extern Queue *_buf_block;
 
 // Kernel Module Functions
 void _ps2_keyboard_init( void );
-void _ps2_keyboard_ready( void );
-void _ps2_keyboard_clear( void );
 void _ps2_keyboard_isr( int vec, int code );
-Uint _ps2_keyboard_read( void );
-void _ps2_keyboard_write( Uint command );
 Uint8 _ps2_get_io_req( void );
 void _ps2_delete_request( Uint8 index );
 int _ps2_search_pid( void *pid1, void *pid2);
